611dbfc9c4a3dc6c22b23bc2a738383c3a04b967
linkin22 <o.ramogi@gmail.com>
2015-11-19 19:49:49 +0300

    commit one

--- app/src/main/java/com/ramogi/xboxme/Constants.java
+++ app/src/main/java/com/ramogi/xboxme/Constants.java
@@ -4,4 +4,12 @@ package com.ramogi.xboxme;
  * Created by ROchola on 11/19/2015.
  */
 public class Constants {
+
+    public static final String PROJECT_ID = "mschoolapp";
+    public static final String PROJECT_NO = "776511346301";
+    public static final String WEB_CLIENT_ID = "776511346301-s2c3nb039u4k9upn3tvmlnopdm5e2t9k.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID = "776511346301-qp3ih1tnrvig0e62m97ddbh05e1ol194.apps.googleusercontent.com";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+    public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+
 }
--- xboxmebackend/src/main/java/com/ramogi/xbox/backend/GamersLocation.java
+++ xboxmebackend/src/main/java/com/ramogi/xbox/backend/GamersLocation.java
@@ -1,7 +1,70 @@
 package com.ramogi.xbox.backend;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import java.util.Date;
+
 /**
  * Created by ROchola on 11/19/2015.
  */
+@Entity
 public class GamersLocation {
+
+    @Id
+    private String email;
+    private String displayname;
+    private Date txntime;
+    private double latx;
+    private double longy;
+    private String gamertag;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public Date getTxntime() {
+        return txntime;
+    }
+
+    public void setTxntime(Date txntime) {
+        this.txntime = txntime;
+    }
+
+    public double getLatx() {
+        return latx;
+    }
+
+    public void setLatx(double latx) {
+        this.latx = latx;
+    }
+
+    public double getLongy() {
+        return longy;
+    }
+
+    public void setLongy(double longy) {
+        this.longy = longy;
+    }
+
+    public String getGamertag() {
+        return gamertag;
+    }
+
+    public void setGamertag(String gamertag) {
+        this.gamertag = gamertag;
+    }
 }
